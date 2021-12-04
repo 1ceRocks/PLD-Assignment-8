72980d52da1d61ea11157cbf81786dd142c9b861
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-12-05 00:36:44 +0800

    Added multiple modules (os, sys, time, winsound) for a more complex and best complementary processing of the Villariza Lottery Program.

--- prog1-assignment8-villariza.py
+++ prog1-assignment8-villariza.py
@@ -7,8 +7,31 @@
 # If the user enter “y” the user will play again
 # if “n” the program will exit.
 
-import random
+import os, random, sys, time, winsound
 verRepeat = False
+os.system('cls')
+
+#
+progGreet = "{}".format(f"\n$! Welcome to Villariza Lottery !$\n")
+progInfo = "{}".format(f"\nPROGRAM INFORMATION AND INSTRUCTIONS")
+prog_varA = "{}".format(f"\nA. You just simply have to input THREE (3) random numbers ranging from 0-9 on the terminal.")
+prog_varB = '{varA} e.g. {varB}'.format(varA = f"\nB. The Program will input THREE (3) Random Winning Numbers regarded on the Lottery Menu.", varB = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)])
+prog_varC = "{}".format(f"\nC. If the Random Winning Numbers matches with your Numerical Input, then the program output will respond saying \"You Win!\"")
+prog_varD = "{}".format(f"\nD. If the Random Winning Numbers does not match with your Numerical Input, then the program output will respond saying \"You've Lost.\"")
+
+print(progGreet, progInfo, prog_varA, prog_varB, prog_varC, prog_varD)
+
+#
+num_dataInval = '{}'.format("\nNumerical inputs should range only on 0-9. \n>>> ")
+alnum_printError = '{}'.format("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+RPT_printError = '{}'.format("\nInvalid Input. Please try again by answering Y / N (Yes / No) \nY , N >>> ")
+
+#
+data1_Inpt = '{}'.format("\nEnter your FIRST (1st) number on the space provided below.")
+data2_Inpt = '{}'.format("\nEnter your SECOND (2nd) number on the space provided below.")
+data3_Inpt = '{}'.format("\nEnter your THIRD (3rd) number on the space provided below.")
+configRPT_Inpt = '{}'.format("\nTry again? Answer only in characters Y / N (Yes / No) \nY , N >>> ")
+countdownSec = 3
 
 while not(verRepeat):
     try:
@@ -20,73 +43,127 @@ while not(verRepeat):
         inval_DGT = False
         inval1 = True; inval2 = True; inval3 = True
         while not(inval_DGT):
-            data1 = input("\nEnter your FIRST number \n>>> ")
+            data1 = input(f"{data1_Inpt}   [N/A, N/A, N/A] \n>>> ")
             while inval1:
                 if len(data1) >= 2:
                     if (data1.isalpha() == True) or (data1.isprintable() == False) or (data1.isspace() == True):
-                        data1 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data1 = input(f"\nFIRST (1st) Number Input \n{alnum_printError}")
                         continue;
-                    elif data1.replace("!@#$%^&*()_+[];',./?","", 20).isdigit():
-                        if len(data1) >= 2:
-                            data1 = input("\nNumerical inputs should range only on 0-9. \n>>> ")
-                            continue;
+                    elif data1.isdigit() == True:
+                        for beta in data1:
+                            if beta in "0":
+                                if len(data1) >= 3:
+                                    os.system('cls')
+                                    data1 = input(f"\nFIRST (1st) Number Input \n{num_dataInval}")
+                                    break;
+                                else:
+                                    inval1 = False
+                                    break;
+                            else:
+                                continue;
+                        if inval1 == False:
+                            break;
                     else:
-                        data1 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data1 = input(f"\nFIRST (1st) Number Input \n{alnum_printError}")
                         continue;
                 elif len(data1) == 1:
                     if data1.isspace():
-                        data1 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data1 = input(f"\nFIRST (1st) Number Input \n{alnum_printError}")
                         continue;
                     else:
-                        inval1 = True
+                        inval1 = False
                         break;
-            data2 = input("\nEnter your SECOND number: \n>>> ")
+            os.system('cls')
+            data2 = input(f"{data2_Inpt}  [{int(data1)}, N/A, N/A] \n>>> ")
             while inval2:
                 if len(data2) >= 2:
                     if (data2.isalpha() == True) or (data2.isprintable() == False) or (data2.isspace() == True):
-                        data2 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data2 = input(f"\nSECOND (2nd) Number Input \n{alnum_printError}")
                         continue;
-                    elif data2.replace("!@#$%^&*()_+[];',./?","", 20).isdigit():
-                        if len(data2) >= 2:
-                            data2 = input("\nNumerical inputs should range only on 0-9. \n>>> ")
-                            continue;
+                    elif data2.isdigit() == True:
+                        for beta in data2:
+                            if beta in "0":
+                                if len(data2) >= 3:
+                                    os.system('cls')
+                                    data2 = input(f"\nSECOND (2nd) Number Input \n{num_dataInval}")
+                                    break;
+                                else:
+                                    inval2 = False
+                                    break;
+                            else:
+                                continue;
+                        if inval2 == False:
+                            break;
                     else:
-                        data2 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data2 = input(f"\nSECOND (2nd) Number Input \n{alnum_printError}")
                         continue;
                 elif len(data2) == 1:
                     if data2.isspace():
-                        data2 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data2 = input(f"\nSECOND (2nd) Number Input \n{alnum_printError}")
                         continue;
                     else:
-                        inval2 = True
+                        inval2 = False
                         break;
-            data3 = input("\nEnter your THIRD number: \n>>> ")
+            os.system('cls')
+            data3 = input(f"{data3_Inpt}   [{int(data1)}, {int(data2)}, N/A] \n>>> ")
             while inval3:
                 if len(data3) >= 2:
                     if (data3.isalpha() == True) or (data3.isprintable() == False) or (data3.isspace() == True):
-                        data3 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data3 = input(f"\nTHIRD (3rd) Number Input \n{alnum_printError}")
                         continue;
-                    elif data3.replace("!@#$%^&*()_+[];',./?","", 20).isdigit():
-                        if len(data3) >= 2:
-                            data3 = input("\nNumerical inputs should range only on 0-9. \n>>> ")
-                            continue;
+                    elif data3.isdigit() == True:
+                        for beta in data3:
+                            if beta in "0":
+                                if len(data3) >= 3:
+                                    os.system('cls')
+                                    data3 = input(f"\nTHIRD (3rd) Number Input \n{num_dataInval}")
+                                    break;
+                                else:
+                                    inval3 = False
+                                    break;
+                            else:
+                                continue;
+                        if inval3 == False:
+                            break;
                     else:
-                        data3 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data3 = input(f"\nTHIRD (3rd) Number Input \n{alnum_printError}")
                         continue;
                 elif len(data3) == 1:
                     if data3.isspace():
-                        data3 = input("\nDo not include any unnecessary input/s on the program. \nPlease try again by typing and entering a numerical input that should only range from 0-9. \n>>> ")
+                        os.system('cls')
+                        data3 = input(f"\nTHIRD (3rd) Number Input \n{alnum_printError}")
                         continue;
                     else:
-                        inval3 = True
+                        inval3 = False
                         break;
 
             fnl_data = [int(data1), int(data2), int(data3)]
             inval_DGT = True
             break;
 
-        if fnl_data == fnl_win:
-            print(f"\n{fnl_data} = {fnl_win} | Winner!\n")
+        os.system('cls')
+        print(f"\nPython will match it now! Did you win or not?\nYour Numbers: [{int(data1)}, {int(data2)}, {int(data3)}]\n")
+
+        if sorted(fnl_data, reverse = True) == sorted(fnl_win, reverse = True):
+            for beta in range(countdownSec):
+                if beta == 0:
+                    print("Matching in    " + str(countdownSec - beta) + " ...")
+                elif beta == 1:
+                    print("Evaluating in  " + str(countdownSec - beta) + " ...")
+                elif beta == 2:
+                    print("Revealing in   " + str(countdownSec - beta) + " ...")
+                time.sleep(10)
+            os.system('cls')
+            print(f"\nYour Num: {fnl_data} = Lotto Num: {fnl_win} | You Win!")
+            winsound.PlaySound("Hello", winsound.SND_FILENAME)
             configRPT = input("\nTry again y/n \n>>> ")
             inval_IPT = False
             while not(inval_IPT):
@@ -99,7 +176,7 @@ while not(verRepeat):
                     inval_IPT = True
                     continue;
                 else:
-                    configRPT = input(f"\nInvalid Input. Please try again by answering y/n \n>>> ")
+                    configRPT = input(f"{configRPT_Inpt}")
                     inval_IPT = False
                     break;
             if (verRepeat == True):
@@ -109,8 +186,18 @@ while not(verRepeat):
         elif verRepeat == True:
             break;
         else:
-            print(f"\n{fnl_data} = {fnl_win} | You loss\n")
-            configRPT = input("\nTry again y/n \n>>> ")
+            for beta in range(countdownSec):
+                if beta == 0:
+                    print("Matching in    " + str(countdownSec - beta) + " ...")
+                elif beta == 1:
+                    print("Evaluating in  " + str(countdownSec - beta) + " ...")
+                elif beta == 2:
+                    print("Revealing in   " + str(countdownSec - beta) + " ...")
+                time.sleep(1)
+            os.system('cls')
+            print(f"\nYour Num: {fnl_data} = Lotto Num: {fnl_win} | You've Lost.")
+            winsound.PlaySound("Hello", winsound.SND_FILENAME)
+            configRPT = input(f"{configRPT_Inpt}")
             inval_IPT = False
             while not(inval_IPT):
                 if configRPT.upper().replace(".,?!","",10) == "Y":
@@ -122,7 +209,7 @@ while not(verRepeat):
                     inval_IPT = True
                     continue;
                 else:
-                    configRPT = input(f"\nInvalid Input. Please try again by answering y/n \n>>> ")
+                    configRPT = input(f"{RPT_printError}")
                     inval_IPT = False
             if (verRepeat == True):
                 break;
@@ -133,4 +220,9 @@ while not(verRepeat):
         progError = '{0}'.format(delta)
         print(f"Invalid input. | {progError}")
 
-print("\nOkay! You may now exit the game.\n")
+    finally:
+        None
+
+os.system('cls')
+print("\nThank you for playing $! Villariza Lottery $! \nWe'll see you again next time! \n\nPython Info: The program has been closed.\n")
+sys.exit()
